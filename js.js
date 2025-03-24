b2a9fc3404f2e906488413373bcbb1c826055c55
Arsenii Penkovski <Penkovskiy2024@mail.ru>
2025-03-24 21:02:04 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,38 @@
+// let b = new Object();
+// let b  = {}
+
+// let studentlist{
+//     name:Егор;
+//     group: Python47;
+// }
+
+// alert(studentList.name)
+// //добавляем в оьбект ключ и значение
+// studentList.surname = "Янюк"
+// //удаление
+// delete studentList.name;
+
+// let studentList = {
+//     name: "Егор"
+//     group: "Python47"
+// }
+
+
+// //задание 
+// let list = {
+//     food:"Apple",
+//     group:"food/drink"
+// }
+// console.log(list)
+// let x = +prompt("1 - добавить, 2 - удалить")
+// if (x === 1){
+//     let y = prompt("Введите группу")
+//     let b = prompt("Выберите еду")
+//     list[y] = b
+//     console.log(list)
+// }else if (x ===2 ){
+//     console.log(list)
+//     let deleteKey = prompt("Введите группу")
+//     delete list[deleteKey]
+//     console.log(list)
+// }
